f424aaaf0f5ecd3708276281700ba8cf5716ef22
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2022-07-25 15:00:09 +0700

    refactor: server setup

--- cmd/patient-api/main.go
+++ cmd/patient-api/main.go
@@ -1,18 +1,10 @@
 package main
 
 import (
-	"context"
-	"errors"
-	"fmt"
-	"github.com/gin-gonic/gin"
 	"github.com/synthia-telemed/backend-api/pkg/config"
 	"github.com/synthia-telemed/backend-api/pkg/logger"
+	"github.com/synthia-telemed/backend-api/pkg/server"
 	"log"
-	"net/http"
-	"os"
-	"os/signal"
-	"syscall"
-	"time"
 )
 
 func main() {
@@ -28,39 +20,6 @@ func main() {
 	defer zapLogger.Sync()
 	sugaredLogger := zapLogger.Sugar()
 
-	gin.SetMode(cfg.GinMode)
-	router := gin.New()
-	router.Use(gin.Recovery())
-	router.Use(gin.LoggerWithConfig(gin.LoggerConfig{SkipPaths: []string{"/api/healthcheck"}}))
-	router.GET("/api/healthcheck", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"success":   true,
-			"timestamp": time.Now(),
-		})
-	})
-
-	srv := &http.Server{
-		Addr:    fmt.Sprintf(":%d", cfg.Port),
-		Handler: router,
-	}
-
-	go func() {
-		sugaredLogger.Infow("Starting server", "addr", srv.Addr)
-		if err := srv.ListenAndServe(); err != nil && errors.Is(err, http.ErrServerClosed) {
-			sugaredLogger.Fatalw("Failed to start server", "error", err)
-		}
-	}()
-
-	quit := make(chan os.Signal)
-	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
-	<-quit
-	sugaredLogger.Info("Shutting down server...")
-
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-
-	if err := srv.Shutdown(ctx); err != nil {
-		sugaredLogger.Fatalw("Server forced to shutdown", "error", err)
-	}
-	sugaredLogger.Info("Server exiting")
+	ginServer := server.NewGinServer(cfg, sugaredLogger)
+	ginServer.ListenAndServe()
 }
---
+++ pkg/server/gin.go
@@ -0,0 +1,67 @@
+package server
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/synthia-telemed/backend-api/pkg/config"
+	"go.uber.org/zap"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+type Server struct {
+	*gin.Engine
+	logger *zap.SugaredLogger
+	addr   string
+}
+
+func NewGinServer(cfg *config.Config, logger *zap.SugaredLogger) *Server {
+	gin.SetMode(cfg.GinMode)
+	router := gin.New()
+	router.Use(gin.Recovery())
+	router.Use(gin.LoggerWithConfig(gin.LoggerConfig{SkipPaths: []string{"/api/healthcheck"}}))
+	router.GET("/api/healthcheck", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"success":   true,
+			"timestamp": time.Now(),
+		})
+	})
+
+	return &Server{
+		Engine: router,
+		logger: logger,
+		addr:   fmt.Sprintf(":%d", cfg.Port),
+	}
+}
+
+func (s Server) ListenAndServe() {
+	srv := &http.Server{
+		Addr:    s.addr,
+		Handler: s.Engine,
+	}
+
+	go func() {
+		s.logger.Infow("Starting server", "addr", srv.Addr)
+		if err := srv.ListenAndServe(); err != nil && errors.Is(err, http.ErrServerClosed) {
+			s.logger.Infow("Failed to start server", "error", err)
+		}
+	}()
+
+	quit := make(chan os.Signal)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	s.logger.Info("Shutting down server...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		s.logger.Fatalw("Server forced to shutdown", "error", err)
+	}
+	s.logger.Info("Server exiting")
+}
