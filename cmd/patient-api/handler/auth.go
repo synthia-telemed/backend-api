4625efe33fad9b2d1158fd6291e51c264a577a81
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2022-08-11 20:13:13 +0700

    add: auth and error

---
+++ cmd/patient-api/handler/auth.go
@@ -0,0 +1,47 @@
+package handler
+
+import (
+	sentrygin "github.com/getsentry/sentry-go/gin"
+	"github.com/gin-gonic/gin"
+	. "github.com/synthia-telemed/backend-api/pkg/datastore"
+	"net/http"
+)
+
+type AuthHandler struct {
+	patientDataStore PatientDataStore
+}
+
+func NewAuthHandler(patientDataStore PatientDataStore) *AuthHandler {
+	return &AuthHandler{patientDataStore}
+}
+
+func (h AuthHandler) Login(c *gin.Context) {
+	var req LoginRequest
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, ErrorResponse{err.Error()})
+		return
+	}
+
+	patient, err := h.patientDataStore.FindByNationalID(req.NationalID)
+	if err != nil {
+		if hub := sentrygin.GetHubFromContext(c); hub != nil {
+			hub.CaptureException(err)
+		}
+		c.AbortWithStatusJSON(http.StatusInternalServerError, ErrorResponse{err.Error()})
+		return
+	}
+
+	if patient == nil {
+		// TODO: create new patient
+	}
+
+	// TODO: send OTP to patient's phone number
+}
+
+type LoginRequest struct {
+	NationalID string `json:"national_id" binding:"required,len=13"`
+}
+
+type LoginResponse struct {
+	PhoneNumber string `json:"phone_number"`
+}
