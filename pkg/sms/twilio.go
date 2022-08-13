896b0d7d8378bdab812a0e9358a5716feb7e3720
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2022-08-13 11:38:03 +0700

    fix: twilio sms

--- pkg/sms/twilio.go
+++ pkg/sms/twilio.go
@@ -1,6 +1,7 @@
 package sms
 
 import (
+	"fmt"
 	"github.com/twilio/twilio-go"
 	openapi "github.com/twilio/twilio-go/rest/api/v2010"
 )
@@ -10,23 +11,30 @@ type Client interface {
 }
 
 type TwilioClient struct {
-	client *twilio.RestClient
+	client     *twilio.RestClient
+	fromNumber string
 }
 
-func NewTwilioClient(accountSid, apiKey, apiSecret string) *TwilioClient {
+func NewTwilioClient(accountSid, apiKey, apiSecret, fromNumber string) *TwilioClient {
 	return &TwilioClient{
 		client: twilio.NewRestClientWithParams(twilio.ClientParams{
 			Username:   apiKey,
 			Password:   apiSecret,
 			AccountSid: accountSid,
 		}),
+		fromNumber: fromNumber,
 	}
 }
 
 func (c TwilioClient) Send(to, body string) error {
 	params := &openapi.CreateMessageParams{}
-	params.SetTo(to)
+	params.SetTo(c.parseThaiPhoneNumber(to))
 	params.SetBody(body)
+	params.SetFrom(c.fromNumber)
 	_, err := c.client.Api.CreateMessage(params)
 	return err
+}
+
+func (c TwilioClient) parseThaiPhoneNumber(phoneNumber string) string {
+	return fmt.Sprintf("+66%s", phoneNumber[1:])
 }
