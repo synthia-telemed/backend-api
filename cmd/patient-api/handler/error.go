06045ffc6c999f5eb059806515654a906dfcaceb
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2022-08-13 11:37:48 +0700

    add: error handler func

--- cmd/patient-api/handler/error.go
+++ cmd/patient-api/handler/error.go
@@ -1,5 +1,20 @@
 package handler
 
+import (
+	sentrygin "github.com/getsentry/sentry-go/gin"
+	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
+	"net/http"
+)
+
 type ErrorResponse struct {
 	Message string `json:"message"`
+}
+
+func InternalServerError(c *gin.Context, logger *zap.SugaredLogger, err error, message string) {
+	if hub := sentrygin.GetHubFromContext(c); hub != nil {
+		hub.CaptureException(err)
+	}
+	logger.Errorw(message, "error", err)
+	c.AbortWithStatusJSON(http.StatusInternalServerError, ErrorResponse{err.Error()})
 }
