85cdcfe1bfdaf993b7e84f2ebe419b6275989392
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2022-08-13 15:21:50 +0700

    add: redis cache client (#3)

---
+++ pkg/cache/redis.go
@@ -0,0 +1,47 @@
+package cache
+
+import (
+	"context"
+	"crypto/tls"
+	"github.com/go-redis/redis/v8"
+	"time"
+)
+
+type Client interface {
+	Get(ctx context.Context, key string) (string, error)
+	Set(ctx context.Context, key string, value string, expiredIn time.Duration) error
+}
+
+type Config struct {
+	Endpoint  string `env:"REDIS_HOST,required"`
+	Username  string `env:"REDIS_USERNAME" envDefault:""`
+	Password  string `env:"REDIS_PASSWORD" envDefault:""`
+	TLSEnable bool   `env:"REDIS_TLS_ENABLE" envDefault:"false"`
+}
+
+type RedisClient struct {
+	client *redis.Client
+}
+
+func NewRedisClient(config *Config) *RedisClient {
+	otp := &redis.Options{
+		Addr:     config.Endpoint,
+		Username: config.Username,
+		Password: config.Password,
+	}
+	if config.TLSEnable {
+		otp.TLSConfig = &tls.Config{}
+	}
+
+	return &RedisClient{
+		client: redis.NewClient(otp),
+	}
+}
+
+func (c RedisClient) Get(ctx context.Context, key string) (string, error) {
+	return c.client.Get(ctx, key).Result()
+}
+
+func (c RedisClient) Set(ctx context.Context, key string, value string, expiredIn time.Duration) error {
+	return c.client.Set(ctx, key, value, expiredIn).Err()
+}
