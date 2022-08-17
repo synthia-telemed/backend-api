38ccafab12ed6e41158806b61aa1d0062b39f77b
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2022-08-17 20:24:44 +0700

    fix: circular dependency

--- cmd/patient-api/handler/middleware/parser.go
+++ cmd/patient-api/handler/middleware/parser.go
@@ -2,25 +2,19 @@ package middleware
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/synthia-telemed/backend-api/cmd/patient-api/handler"
 	"net/http"
 	"strconv"
-)
-
-var (
-	ErrMissingPatientID = handler.ErrorResponse{Message: "Missing patient ID"}
-	ErrInvalidPatientID = handler.ErrorResponse{Message: "Invalid patient ID"}
 )
 
 func ParsePatientID(c *gin.Context) {
 	id := c.Request.Header.Get("X-USER-ID")
 	if id == "" {
-		c.AbortWithStatusJSON(http.StatusBadRequest, ErrMissingPatientID)
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"message": "Missing patient ID"})
 		return
 	}
 	uintID, err := strconv.ParseUint(id, 10, 32)
 	if err != nil {
-		c.AbortWithStatusJSON(http.StatusBadRequest, ErrInvalidPatientID)
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"message": "Invalid patient ID"})
 		return
 	}
 	c.Set("patientID", uint(uintID))
