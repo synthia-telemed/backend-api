31d5f3ece83b28b2c2c80724af9ae28886920d74
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2022-08-18 17:42:53 +0700

    [SYN-80] hotfix/swagger JWS auth is missing (#23)
    * add: CreateCustomerWithCard
    
    * fix: received config from param
    
    * add: .env.test
    
    * add: Omise addCustomer unit test
    
    * add: omise env
    
    * use test env for datastore
    
    * set: default env
    
    * add HOSPITAL_SYS_ENDPOINT
    
    * use: config from env
    
    * [SYN-57] Credit card adding handler (#16)
    
    * Move error to error.go
    
    * add: update patient
    
    * change createwithcard to just create customer
    
    * add: addCreditCard func
    
    * add: AddCreditCard handler
    
    * add: register payment handler
    
    * add: mock payment
    
    * use: parser middleware
    
    * fix: circular dependency
    
    * fix: status not set
    
    * add: first time adding credit card test cases
    
    * add: testing find patient error
    
    * add: existing customer credit card adding
    
    * add: adding failed test case
    
    * remove gin setmode
    
    * add GIN_MODE
    
    * [SYN-58] List credit cards function (#17)
    
    * add: list card func
    
    * add: list credit card test case
    
    * use: Succeed()
    
    * use: Succeed()
    
    * [SYN-75] Add credit card listing API (#18)
    
    * add: GetCreditCards handler
    
    * add: Get patient's credit cards test cases
    
    * [SYN-59] Feat/pay by credit card (#19)
    
    * add: PayWithCreditCard func
    
    * add: pay with credit card testcase
    
    * add: clean up
    
    * add: seed
    
    * add: failure
    
    * add: IsOwnCreditCard
    
    * refactor: return cardID
    
    * add: number parameter
    
    * add: credit card charing error test cases
    
    * [SYN-60] Add swagger to credit card list and add (#20)
    
    * fix: return 401 instead of 400
    
    * add: credit card CR swagger
    
    * [SYN-79] ADD default card field (#21)
    
    * add: default card field
    
    * update assertion
    
    * swagger gen
    
    * add: JWS Token authentication
    
    * add: /patient basepath

--- docs/docs.go
+++ docs/docs.go
@@ -113,6 +113,9 @@ const docTemplate = `{
                 "security": [
                     {
                         "UserID": []
+                    },
+                    {
+                        "JWSToken": []
                     }
                 ],
                 "tags": [
@@ -147,6 +150,9 @@ const docTemplate = `{
                 "security": [
                     {
                         "UserID": []
+                    },
+                    {
+                        "JWSToken": []
                     }
                 ],
                 "tags": [
@@ -267,6 +273,11 @@ const docTemplate = `{
         }
     },
     "securityDefinitions": {
+        "JWSToken": {
+            "type": "apiKey",
+            "name": "Authorization",
+            "in": "header"
+        },
         "UserID": {
             "type": "apiKey",
             "name": "X-USER-ID",
@@ -279,7 +290,7 @@ const docTemplate = `{
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0.0",
 	Host:             "",
-	BasePath:         "/api",
+	BasePath:         "/patient/api",
 	Schemes:          []string{},
 	Title:            "Synthia Patient Backend API",
 	Description:      "This is a Synthia patient backend API.",
