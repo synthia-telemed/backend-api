4d73082ec7765178aa6ef96dc419338edef89025
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2022-08-24 17:14:33 +0700

    add: NewErrorResponse func

--- pkg/server/error.go
+++ pkg/server/error.go
@@ -19,6 +19,10 @@ type ErrorResponse struct {
 	Message string `json:"message"`
 }
 
+func NewErrorResponse(msg string) *ErrorResponse {
+	return &ErrorResponse{Message: msg}
+}
+
 func InternalServerError(c *gin.Context, logger *zap.SugaredLogger, err error, message string) {
 	if hub := sentrygin.GetHubFromContext(c); hub != nil {
 		hub.CaptureException(err)
