ec1edb8324c9c0ea48dfda6e84b15a7d7dbba097
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2022-09-07 23:09:27 +0700

    [SYN-140] Chore/hospital sys test container (#66)
    * add: test container helper
    
    * use: common test_container in cache_suite_test.go
    
    * refactor to use common setup
    
    * add: setup pg and hospital sys
    
    * change the test i/o
    
    * add: more
    
    * add: PaidInvoice testcase
    
    * add registry login

--- pkg/hospital/hospital_suite_test.go
+++ pkg/hospital/hospital_suite_test.go
@@ -1,6 +1,9 @@
 package hospital_test
 
 import (
+	"github.com/google/uuid"
+	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/wait"
 	"testing"
 
 	. "github.com/onsi/ginkgo/v2"
@@ -10,4 +13,25 @@ import (
 func TestHospital(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "Hospital Suite")
+}
+
+var compose *testcontainers.LocalDockerCompose
+
+var _ = BeforeSuite(func() {
+	setupTestHospitalSystem()
+})
+
+var _ = AfterSuite(func() {
+	Expect(compose.Down().Error).To(Succeed())
+})
+
+func setupTestHospitalSystem() {
+	id := uuid.New().String()
+	compose = testcontainers.NewLocalDockerCompose([]string{"docker-compose.test.yaml"}, id)
+	execError := compose.
+		WithCommand([]string{"up", "-d"}).
+		WaitForService("postgres", wait.ForLog("database system is ready to accept connections").WithOccurrence(2)).
+		WaitForService("hospital-sys", wait.ForLog("Nest application successfully started")).
+		Invoke()
+	Expect(execError.Error).To(BeNil())
 }
