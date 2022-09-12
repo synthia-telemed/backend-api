d42e2415e74d2bf31872a57f533d592a37fd25fe
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2022-09-12 13:23:48 +0700

    add: appointment datastore

---
+++ pkg/datastore/appointment.go
@@ -0,0 +1,45 @@
+package datastore
+
+import (
+	"errors"
+	"gorm.io/gorm"
+	"time"
+)
+
+type Appointment struct {
+	ID        uint           `json:"id" gorm:"autoIncrement,primaryKey"`
+	CreatedAt time.Time      `json:"created_at"`
+	UpdatedAt time.Time      `json:"updated_at"`
+	DeletedAt gorm.DeletedAt `json:"-" gorm:"index"`
+	RefID     int            `json:"ref_id" gorm:"unique"`
+	Duration  float64        `json:"duration"`
+}
+
+type AppointmentDataStore interface {
+	Create(appointment *Appointment) error
+	FindByRefID(refID int) (*Appointment, error)
+}
+
+type GormAppointmentDataStore struct {
+	db *gorm.DB
+}
+
+func NewGormAppointmentDataStore(db *gorm.DB) (AppointmentDataStore, error) {
+
+	return &GormAppointmentDataStore{db: db}, db.AutoMigrate(&Appointment{})
+}
+
+func (g GormAppointmentDataStore) Create(appointment *Appointment) error {
+	return g.db.Create(appointment).Error
+}
+
+func (g GormAppointmentDataStore) FindByRefID(refID int) (*Appointment, error) {
+	var appointment Appointment
+	if err := g.db.Where("ref_id = ?", refID).First(&appointment).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return nil, nil
+		}
+		return nil, err
+	}
+	return &appointment, nil
+}
