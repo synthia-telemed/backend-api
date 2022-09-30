d5cc9df223820d2709e44ea981ed41bb6f02a6a3
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2022-09-30 11:49:27 +0700

    add: SetCreditCardIsDefaultRequest struct

--- cmd/patient-api/handler/payment.go
+++ cmd/patient-api/handler/payment.go
@@ -155,6 +155,7 @@ type SetCreditCardIsDefaultRequest struct {
 // @Summary      Set isDefault status of credit card
 // @Tags         Payment
 // @Param  		 cardID 	path	 integer 	true "ID of the credit card to set isDefault"
+// @Param 	  	 SetCreditCardIsDefaultRequest body SetCreditCardIsDefaultRequest true "IsDefault status of the credit card"
 // @Success      200
 // @Failure      400  {object}  server.ErrorResponse "Invalid credit card ID"
 // @Failure      401  {object}  server.ErrorResponse "Unauthorized"
