e82218e286ab88ed293d2211a94f8dd6ea6ed11a
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2022-10-27 21:39:41 +0700

    add: get name handler with test

--- cmd/patient-api/handler/info.go
+++ cmd/patient-api/handler/info.go
@@ -1,31 +1,60 @@
 package handler
 
 import (
+	"context"
+	"errors"
 	"github.com/gin-gonic/gin"
+	"github.com/synthia-telemed/backend-api/pkg/datastore"
 	"github.com/synthia-telemed/backend-api/pkg/hospital"
-	"github.com/synthia-telemed/backend-api/pkg/server"
+	"github.com/synthia-telemed/backend-api/pkg/server/middleware"
 	"go.uber.org/zap"
+	"net/http"
 )
 
 type InfoHandler struct {
 	hospitalClient hospital.SystemClient
-	logger         *zap.SugaredLogger
-	*server.GinHandler
+	PatientGinHandler
 }
 
-func NewInfoHandler(hospitalClient hospital.SystemClient, logger *zap.SugaredLogger) *InfoHandler {
+func NewInfoHandler(patientDataStore datastore.PatientDataStore, hospitalClient hospital.SystemClient, logger *zap.SugaredLogger) *InfoHandler {
 	return &InfoHandler{
-		hospitalClient: hospitalClient,
-		logger:         logger,
-		GinHandler:     &server.GinHandler{Logger: logger},
+		hospitalClient:    hospitalClient,
+		PatientGinHandler: NewPatientGinHandler(patientDataStore, logger),
 	}
 }
 
+func (h InfoHandler) Register(r *gin.RouterGroup) {
+	g := r.Group("info")
+	g.GET("/name", middleware.ParseUserID, h.ParsePatient, h.GetName)
+}
+
 type GetNameResponse struct {
-	EN hospital.Name `json:"EN"`
-	TH hospital.Name `json:"TH"`
+	EN *hospital.Name `json:"EN"`
+	TH *hospital.Name `json:"TH"`
 }
 
 func (h InfoHandler) GetName(c *gin.Context) {
-
+	rawPatient, exist := c.Get("Patient")
+	if !exist {
+		h.InternalServerError(c, errors.New("c.Get Patient not exist"), "c.Get Patient not exist")
+		return
+	}
+	patient, ok := rawPatient.(*datastore.Patient)
+	if !ok {
+		h.InternalServerError(c, errors.New("patient type casting error"), "Patient type casting error")
+		return
+	}
+	patientInfo, err := h.hospitalClient.FindPatientByID(context.Background(), patient.RefID)
+	if err != nil {
+		h.InternalServerError(c, err, "h.hospitalClient.FindPatientByID error")
+		return
+	}
+	if patientInfo == nil {
+		c.JSON(http.StatusNotFound, ErrPatientNotFound)
+		return
+	}
+	c.JSON(http.StatusOK, &GetNameResponse{
+		EN: patientInfo.NameEN,
+		TH: patientInfo.NameTH,
+	})
 }
