5d5eedd776b923df3787fd8e31cb87b9b212514f
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2022-10-28 20:25:40 +0700

    add: list notification

---
+++ cmd/patient-api/handler/notification.go
@@ -0,0 +1,35 @@
+package handler
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/synthia-telemed/backend-api/pkg/datastore"
+	"go.uber.org/zap"
+	"net/http"
+)
+
+type NotificationHandler struct {
+	notificationDataStore datastore.NotificationDataStore
+	PatientGinHandler
+}
+
+func NewNotificationHandler(notificationDataStore datastore.NotificationDataStore, patientDataStore datastore.PatientDataStore, logger *zap.SugaredLogger) *NotificationHandler {
+	return &NotificationHandler{
+		notificationDataStore: notificationDataStore,
+		PatientGinHandler:     NewPatientGinHandler(patientDataStore, logger),
+	}
+}
+
+func (h NotificationHandler) Register(r *gin.RouterGroup) {
+	g := r.Group("/notification")
+	g.GET("", h.ParseUserID, h.ListNotifications)
+}
+
+func (h NotificationHandler) ListNotifications(c *gin.Context) {
+	patientID := h.GetUserID(c)
+	notifications, err := h.notificationDataStore.ListLatest(patientID)
+	if err != nil {
+		h.InternalServerError(c, err, "h.notificationDataStore.ListLatest error")
+		return
+	}
+	c.JSON(http.StatusOK, notifications)
+}
