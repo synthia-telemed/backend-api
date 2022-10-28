ee7dc72a7b3a249a0f43be5ee35da6b45f167380
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2022-10-28 22:18:22 +0700

    add: read and readAll api handler

--- cmd/patient-api/handler/notification.go
+++ cmd/patient-api/handler/notification.go
@@ -3,8 +3,15 @@ package handler
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/synthia-telemed/backend-api/pkg/datastore"
+	"github.com/synthia-telemed/backend-api/pkg/server"
 	"go.uber.org/zap"
 	"net/http"
+	"strconv"
+)
+
+var (
+	ErrInvalidNotificationID = server.NewErrorResponse("Invalid notification id")
+	ErrNotificationNotFound  = server.NewErrorResponse("Notification not found")
 )
 
 type NotificationHandler struct {
@@ -20,9 +27,11 @@ func NewNotificationHandler(notificationDataStore datastore.NotificationDataStor
 }
 
 func (h NotificationHandler) Register(r *gin.RouterGroup) {
-	g := r.Group("/notification")
-	g.GET("", h.ParseUserID, h.ListNotifications)
-	g.GET("/unread", h.ParseUserID, h.CountUnRead)
+	g := r.Group("/notification", h.ParseUserID)
+	g.GET("", h.ListNotifications)
+	g.PATCH("", h.ReadAll)
+	g.GET("/unread", h.CountUnRead)
+	g.PATCH("/:id", h.AuthorizedPatientToNotification, h.Read)
 }
 
 // ListNotifications godoc
@@ -65,4 +74,52 @@ func (h NotificationHandler) CountUnRead(c *gin.Context) {
 		return
 	}
 	c.JSON(http.StatusOK, &CountUnReadNotificationResponse{Count: count})
+}
+
+func (h NotificationHandler) ReadAll(c *gin.Context) {
+	patientID := h.GetUserID(c)
+	if err := h.notificationDataStore.SetAllAsRead(patientID); err != nil {
+		h.InternalServerError(c, err, "h.notificationDataStore.SetAllAsRead error")
+		return
+	}
+	c.AbortWithStatus(http.StatusOK)
+}
+
+func (h NotificationHandler) AuthorizedPatientToNotification(c *gin.Context) {
+	patientID := h.GetUserID(c)
+	notificationIDStr := c.Param("id")
+	if notificationIDStr == "" {
+		c.AbortWithStatusJSON(http.StatusBadRequest, ErrInvalidNotificationID)
+		return
+	}
+	notificationID, err := strconv.ParseUint(notificationIDStr, 10, 32)
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, ErrInvalidNotificationID)
+		return
+	}
+
+	notification, err := h.notificationDataStore.FindByID(uint(notificationID))
+	if err != nil {
+		h.InternalServerError(c, err, "h.notificationDataStore.FindByID error")
+		return
+	}
+	if notification == nil {
+		c.AbortWithStatusJSON(http.StatusNotFound, ErrNotificationNotFound)
+		return
+	}
+	if notification.PatientID != patientID {
+		c.AbortWithStatusJSON(http.StatusForbidden, ErrForbidden)
+		return
+	}
+	c.Set("Notification", notification)
+}
+
+func (h NotificationHandler) Read(c *gin.Context) {
+	rawNotification, _ := c.Get("Notification")
+	notification, _ := rawNotification.(*datastore.Notification)
+	if err := h.notificationDataStore.SetAsRead(notification.ID); err != nil {
+		h.InternalServerError(c, err, "h.notificationDataStore.SetAsRead error")
+		return
+	}
+	c.AbortWithStatus(http.StatusOK)
 }
